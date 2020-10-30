e7b4e196e9a509956508a928063d6f68e3b63a7a
kennethAdams <kennethjadams95@gmail.com>
2020-10-30 12:57:57 -0400

    initial commit

---
+++ PokedekApp/Model/Pokemon.swift
@@ -0,0 +1,8 @@
+//
+//  Pokemon.swift
+//  PokedekApp
+//
+//  Created by Field Employee on 10/29/20.
+//
+
+import Foundation
---
+++ PokedekApp/Utilities/Network Manager.swift
@@ -0,0 +1,8 @@
+//
+//  Network Manager.swift
+//  PokedekApp
+//
+//  Created by Field Employee on 10/29/20.
+//
+
+import Foundation
